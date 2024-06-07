a452d632b0e7de9bab4e119b00c31e14604b2603
sambowers2022 <bluebuilder99@gmail.com>
2024-06-06 22:22:36 -0500

    Improved CLI

--- projects.py
+++ projects.py
@@ -10,38 +10,47 @@ connector = mysql.connector.connect(
     database='projects'
 )
 cursor = connector.cursor()
-random_query = "SELECT * FROM projects WHERE status IS NULL ORDER BY RAND() LIMIT 1"
+random_query = "SELECT * FROM projects WHERE status IS NULL ORDER BY RAND() LIMIT %s"
 add_query = "INSERT INTO projects (title, description) VALUES (%s, %s)"
 
-def random():
-    cursor.execute(random_query)
-    print(cursor.fetchone())
+def random(num=1):
+    cursor.execute(random_query, (num,))
+    for project in cursor.fetchall():
+        print(f"Title: {project[1]}")
+        print(f"Description: {project[2]}")
+        print()
 
 def add():
     name = input("Project name: ")
     desc = input("Project description: ")
     cursor.execute(add_query, (name, desc))
+    connector.commit()
 
-def input_loop():
-    opt = None
+def input_loop(leaving=False):
     try:
-        opt = str(input("Choose an option (? for help): ")).lower()[0]
-    except IndexError:
-        input_loop() 
+        args = str(input("Choose an option (? for help): ")).lower().split(' ')
     except KeyboardInterrupt:
-        print("\nClosing...")
-        exit(0)
-    if opt == '?':
+        if leaving:
+            # Closing logic here
+            connector.close()
+            print("\nClosing...")
+            exit(0)
+        print("\n^C again to close")
+        input_loop(True)
+    if args[0][0] == '?':
         print("Options:")
         print("[A]dd a new project")
         print("[R]andomly select a project")
         print("[S]earch for a project")
         print("[Q]uit")
         print()
-    elif opt == 'a':
+    elif args[0][0] == 'a':
         add()
-    elif opt == 'r':
-        random()
+    elif args[0][0] == 'r':
+        if len(args) > 1: 
+            random(int(args[1]))
+        else:
+            random()
     input_loop()
      
 input_loop()
